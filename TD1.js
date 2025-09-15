03ff55bf4a7370f623637278e3fffba3c18ef2b9
lylian <montals.lylian2@gmail.com>
2025-09-15 15:41:03 +0200

    exo 3/4 fini

--- TD1.js
+++ TD1.js
@@ -1,12 +1,26 @@
 // exercice 1
-let temp_celsius = 25;
-let temp_fahrenheit = (temp_celsius * 9/5) + 32;
+const temp_celsius = 25;
+const temp_fahrenheit = (temp_celsius * 9/5) + 32;
 
 document.getElementById("resultat1").textContent = 'Température en celsius : ' + temp_celsius + '°C Température en fahrenheit : ' + temp_fahrenheit + '°F';
 
 // exercice 2
-let long = 5;
-let larg = 5;
-let aire = long*larg
+const long = 5;
+const larg = 5;
+const aire = long*larg
 
 document.getElementById("resultat2").textContent = "l'aire d'un rectange de longueur " + long + " et de largeur " + larg + " est de " + aire;
+
+// exercice 3
+const prenom = "billy";
+const nom = "bob";
+const nom_prenom = prenom + " " + nom;
+
+document.getElementById("resultat3").textContent = "la personne s'appelle " + nom_prenom;
+
+// exercice 4
+const montantHT = 10;
+const TVA = 0.2;
+const montantTTC = montantHT * (1+TVA);
+
+document.getElementById("resultat4").textContent = "le montant après taxe de 10euros et de " + montantTTC + "euros";
