6731fe760f5c46a6fd60a5cb59122d7f830af8a4
lylian <montals.lylian2@gmail.com>
2025-09-15 15:26:29 +0200

    exo 2 fini

---
+++ TD1.js
@@ -0,0 +1,12 @@
+// exercice 1
+let temp_celsius = 25;
+let temp_fahrenheit = (temp_celsius * 9/5) + 32;
+
+document.getElementById("resultat1").textContent = 'Température en celsius : ' + temp_celsius + '°C Température en fahrenheit : ' + temp_fahrenheit + '°F';
+
+// exercice 2
+let long = 5;
+let larg = 5;
+let aire = long*larg
+
+document.getElementById("resultat2").textContent = "l'aire d'un rectange de longueur " + long + " et de largeur " + larg + " est de " + aire;
